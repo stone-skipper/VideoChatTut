01aebfc69ce22fe93129bfae27260ed2c754f478
stone-skipper <48980449+stone-skipper@users.noreply.github.com>
2022-04-05 00:26:13 +0800

    setting up

---
+++ frontend/src/Main.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./Home";
+import Mobile from "./Mobile";
+
+const Main = () => {
+  return (
+    <Routes>
+      {/* The Switch decides which component to show based on the current URL.*/}
+      <Route exact path="/home" element={<Home />}></Route>
+      <Route path="/mobile" element={<Mobile />}></Route>
+    </Routes>
+  );
+};
+
+export default Main;
